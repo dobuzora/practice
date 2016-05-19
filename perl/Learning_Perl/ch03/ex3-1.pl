1a9543669bc5cee08212e61922af0897ead19531
Nishisi <e145735@gmail.com>
2016-05-20 00:13:39 +0900

    modifid and add README

--- perl/Learning_Perl/ch03/ex3-1.pl
+++ perl/Learning_Perl/ch03/ex3-1.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 
-my @string_list = <STDIN>;
-@string_list = reverse @string_list;
-print @string_list;
+my @string_list = <STDIN>; # 文字列リストの読み込み
+@string_list = reverse @string_list; # 逆順に並び替え
+print @string_list; # 表示
+
+# print reverse <STDIN>;
--- perl/Learning_Perl/ex3-1.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-
-my @string_list = <STDIN>; # 文字列リストの読み込み
-@string_list = reverse @string_list; # 逆順に並び替え
-print @string_list; # 表示
-
-# print reverse <STDIN>;
