74a10c8b4427fad0d8cbcea28262eb0036ddbd96
Nishisi <e145735@gmail.com>
2016-05-31 23:53:53 +0900

    practice OpenCV

---
+++ python/OpenCV/Tutorials/Image_Processing/Contours/contours.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+im = cv2.imread('cora.jpg')
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,127,255,cv2.THRESH_BINARY)
+
+image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+#img = cv2.drawContours(im, contours, -1, (0,255,0), 3)
+
+cnt = contours[0]
+img = cv2.drawContours(im, [cnt], 0, (0,255,0), 3)
+
+cv2.imshow("test",thresh)
+cv2.waitKey(0)
