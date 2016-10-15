44ae064de0d404b1e5d6f65fb8a2d372039644d9
Nishisi <e145735@gmail.com>
2016-10-15 20:10:26 +0900

    add ch01

---
+++ python/100/ch1/03.py
@@ -0,0 +1,9 @@
+# 03.円周率
+
+str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+new_list = []
+for line in str.split():
+    new_list.append(line[0])
+
+print(new_list)
---
+++ python/100/ch1/04.py
@@ -0,0 +1,5 @@
+# 04.元素記号
+
+str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+
+print(str)
