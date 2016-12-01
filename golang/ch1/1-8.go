cec7149cb366889697492918eb5cfea766c4ea79
Nishisi <e145735@gmail.com>
2016-12-01 21:49:14 +0900

    1-8.go comp

--- golang/ch1/1-8.go
+++ golang/ch1/1-8.go
@@ -7,10 +7,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	prefix := "http://"
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, prefix) {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch:%v", err)
