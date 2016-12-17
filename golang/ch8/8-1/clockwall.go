564e1bca418b60a2de2d57f93a82bcc46d62584f
Nishisi <e145735@gmail.com>
2016-12-17 19:57:47 +0900

    add 8-5

--- golang/ch8/8-1/clockwall.go
+++ golang/ch8/8-1/clockwall.go
@@ -6,13 +6,16 @@ import (
 	"log"
 	"net"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
 )
 
 func main() {
 	if len(os.Args) == 0 {
 		fmt.Println("Fatal")
 	} else {
+		fmt.Println(runtime.NumCPU())
 		var connLists []net.Conn
 		for _, str := range os.Args[1:] {
 			addr := strings.Split(str, "=")
@@ -23,17 +26,23 @@ func main() {
 			fmt.Println(conn)
 			connLists = append(connLists, conn)
 		}
-		go mustCopy(os.Stdout, connLists[0])
-		go mustCopy(os.Stdout, connLists[1])
-		go mustCopy(os.Stdout, connLists[2])
+		var wg sync.WaitGroup
+		wg.Add(3)
+		go mustCopy(&wg, os.Stdout, connLists[0])
+		go mustCopy(&wg, os.Stdout, connLists[1])
+		go mustCopy(&wg, os.Stdout, connLists[2])
+		wg.Wait()
+		//mustCopy(os.Stdin, connLists[2])
+
 		// メイン関数を終わらせないように
-		for {
-		}
+		//for {
+		//}
 	}
 
 }
 
-func mustCopy(dst io.Writer, src io.Reader) {
+func mustCopy(wg *sync.WaitGroup, dst io.Writer, src io.Reader) {
+	defer wg.Done()
 	if _, err := io.Copy(dst, src); err != nil {
 		log.Fatal(err)
 	}
