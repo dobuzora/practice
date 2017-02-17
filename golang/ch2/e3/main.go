4b733052ae866fe3d615b6f9056dd03249da1dbc
Nishisi <Nishiyama@nishiyamasubarushi-no-iMac.local>
2017-02-17 15:21:53 +0900

    Markdown test

--- golang/ch2/e3/main.go
+++ golang/ch2/e3/main.go
@@ -1,20 +1,10 @@
-package popcount
+package main
 
-var pc [256]byte
+import "fmt"
+import "./popcount"
 
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
-
-func PopCount(x uint64) int {
-	return int(pc[byte(x>>(0*8))] +
-		pc[byte(x>>(1*8))] +
-		pc[byte(x>>(2*8))] +
-		pc[byte(x>>(3*8))] +
-		pc[byte(x>>(4*8))] +
-		pc[byte(x>>(5*8))] +
-		pc[byte(x>>(6*8))] +
-		pc[byte(x>>(7*8))])
+func main() {
+	var num uint64 = 3
+	fmt.Println(num)
+	fmt.Println(popcount.PopCount(num))
 }
