75c2c1840ec2bdf55abd9d63f27a90ed0a63ff3b
Nishisi <Nishiyama@nishiyamasubarushi-no-iMac.local>
2017-03-22 02:31:18 +0900

    ch7 ex1

--- golang/ch7/ex1/main.go
+++ golang/ch7/ex1/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
 
 type ByteCounter int
-type WordCOunter int
+type WordCounter int
 type LineCounter int
 
 func (c *ByteCounter) Write(p []byte) (int, error) {
@@ -12,19 +16,38 @@ func (c *ByteCounter) Write(p []byte) (int, error) {
 }
 
 func (w *WordCounter) Write(p []byte) (int, error) {
-
+	*w += WordCounter(len(strings.Split(string(p), " ")))
+	return len(strings.Split(string(p), " ")), nil
 }
 
-func (w *LineCounter) Write(p []byte) (int, error) {
+func (l *LineCounter) Write(p []byte) (int, error) {
+	*l += LineCounter(len(strings.Split(string(p), "\n")))
+	return len(strings.Split(string(p), "\n")), nil
 }
 
 func main() {
-	var c ByteCounter
-	c.Write([]byte("hello"))
-	fmt.Println(c)
+	var (
+		c ByteCounter
+		w WordCounter
+		l LineCounter
+	)
+	str := "hello neko\nneko"
+	log.Println("Call Write method and Set value")
+	fmt.Println("----\n" + str + "\n----")
+	c.Write([]byte(str))
+	w.Write([]byte(str))
+	l.Write([]byte(str))
+	fmt.Printf("c = %v\n", c)
+	fmt.Printf("w = %v\n", w)
+	fmt.Printf("l = %v\n", l)
 
-	c = 0
+	w, l, c = 0, 0, 0
 	var name = "Dolly"
+	fmt.Printf("hello,%s\n", name)
 	fmt.Fprintf(&c, "hello,%s", name)
-	fmt.Println(c)
+	fmt.Fprintf(&w, "hello,%s", name)
+	fmt.Fprintf(&l, "hello,%s", name)
+	fmt.Printf("c = %v\n", c)
+	fmt.Printf("w = %v\n", w)
+	fmt.Printf("l = %v\n", l)
 }
