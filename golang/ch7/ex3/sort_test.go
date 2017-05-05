df536f2c0f921cfdab5abd2345b8243101ab517c
西山昴志 <nishiyama@MacBook-Pro-2.local>
2017-05-05 11:23:47 +0900

    add ch7 ex3

---
+++ golang/ch7/ex3/sort_test.go
@@ -0,0 +1,18 @@
+package treesort
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	data := make([]int, 50)
+	for i := range data {
+		data[i] = rand.Int() % 50
+	}
+	Sort(data)
+	if !sort.IntsAreSorted(data) {
+		t.Errorf("not sorted: %v", data)
+	}
+}
